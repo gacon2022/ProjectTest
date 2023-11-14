2765dc1363be1254508d6f87ce56e42dced5683e
gacon2022 <like.vespa.lx1@gmail.com>
2023-11-14 21:14:18 +0700

    V1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
